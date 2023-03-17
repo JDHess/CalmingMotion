fa5dbb936ae7dc147dda50ed9098152227da2651
Julie Hess <jul>
2023-03-17 16:14:13 -0700

    added spark

--- CalmingMotion/ContentView.swift
+++ CalmingMotion/ContentView.swift
@@ -1,21 +1,51 @@
 //
 //  ContentView.swift
-//  CalmingMotion
+//  Calming Motion
 //
-//  Created by Julie Hess on 3/17/23.
+//  Created by Julie Hess on 12/23/22.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var particleSystem = ParticleSystem()
+    @State private var motionHandler = MotionManager()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        GeometryReader{ geometry in
+            
+            
+            TimelineView(.animation){ timeline in
+                Canvas {context, size in
+                    //drawing code here
+                    let timelineData = timeline.date.timeIntervalSinceReferenceDate
+                    particleSystem.update(date: timelineData)
+                    context.blendMode = .plusLighter
+                    
+                    particleSystem.center = UnitPoint(x: 0.5 + motionHandler.roll, y: 0.5 + motionHandler.pitch)
+                    
+                    //MARK: Change color
+                    
+                    for particle in particleSystem.particles {
+                        let xPos = particle.x * size.width
+                        let yPos = particle.y * size.height
+                        var contextCopy = context
+                        contextCopy.addFilter(.colorMultiply(Color(hue: particle.hue, saturation: 1, brightness: 1)))
+                        contextCopy.opacity = 1-(timelineData - particle.creationDate)
+                        contextCopy.draw(particleSystem.image, at: CGPoint(x: xPos, y: yPos))
+                    }
+                }
+                
+                .gesture(
+                    DragGesture(minimumDistance: 0).onChanged{drag in
+                        particleSystem.center.x = drag.location.x / geometry.size.width
+                        particleSystem.center.y = drag.location.y / geometry.size.height
+                    })
+                .ignoresSafeArea()
+                .background(.black)
+            }
         }
-        .padding()
     }
 }
 
